0bca0e498ae186fa9c946fe924d80c021e6c6d42
Lennert MERTENS <lennert.mertens@nubera.be>
2019-05-07 12:35:57 +0200

    Update PoC scalability

--- broncode/fission-install.sh
+++ broncode/fission-install.sh
@@ -3,29 +3,28 @@
 brew install kubernetes-Helm
 sleep 10 
 
-# Install Fission CLI tools
-curl -Lo fission \
-https://github.com/fission/fission/releases/download/1.2.0/fission-cli-osx \
-&& chmod +x fission && sudo mv fission /usr/local/bin/
-sleep 15
-
 # Deploy Fission to Minikube
 # 1. Initialize Helm
 kubectl -n kube-system create sa tiller && \
 kubectl create clusterrolebinding tiller --clusterrole \
 cluster-admin --serviceaccount=kube-system:tiller
-sleep 10
 
 # 2. Install tiller
 helm init --skip-refresh --upgrade --service-account tiller
-sleep 15
+sleep 20
 
 # 2. Install Fission Framework
 helm repo update && \
 helm install --name fission --namespace fission \
 --set serviceType=NodePort,routerServiceType=NodePort \
 https://github.com/fission/fission/releases/download/1.2.0/fission-all-1.2.0.tgz
-sleep 15
+sleep 20
+
+
+# Install Fission CLI tools
+curl -Lo fission \
+https://github.com/fission/fission/releases/download/1.2.0/fission-cli-osx && \
+chmod +x fission && sudo mv fission /usr/local/bin/
 
 # Install Fission UI
 # 1. Clone the git repository
